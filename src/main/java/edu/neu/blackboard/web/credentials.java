9bc61cda408412b3646d67a5d43bc3c3d4860a8b
tushargl016 <goel.t@husky.neu.edu>
2017-04-04 00:20:46 -0400

    Update credentials.java

--- src/main/java/edu/neu/blackboard/web/credentials.java
+++ src/main/java/edu/neu/blackboard/web/credentials.java
@@ -3,7 +3,7 @@ package edu.neu.blackboard.web;
 import com.amazonaws.auth.BasicAWSCredentials;
 
 public class credentials {
-public static final String access_key_id="AKIAJGLULJR5PPRYMSJA";
-public static final String secret_access_key="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";
+public static final String access_key_id="AKIAJEPUN3YQCGSDQBMQ";
+public static final String secret_access_key="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";
 BasicAWSCredentials awscreds= new BasicAWSCredentials(access_key_id,secret_access_key);
 }
